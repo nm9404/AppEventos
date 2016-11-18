75af053dd24936a7609f1bde9c71590594e799bb
Nicolás Múnera <nicolasmunera@dynamikcollective.com>
2016-11-18 11:26:43 -0500

    Reinicio del sistema

--- Eventos/Fragments/ContactFragment.cs
+++ Eventos/Fragments/ContactFragment.cs
@@ -18,6 +18,13 @@ namespace Eventos.Fragments
     public class ContactFragment : SupportFragment
     {
         DataService dataServiceInstance;
+        TextView eventNameText;
+        TextView eventDateText;
+        TextView eventAddressText;
+        TextView eventDescriptionText;
+        TextView eventHashTagText;
+        TextView eventEmailText;
+        TextView eventNumberText;
 
         public override void OnCreate(Bundle savedInstanceState)
         {
@@ -31,7 +38,7 @@ namespace Eventos.Fragments
             // Use this to return your custom view for this Fragment
             // return inflater.Inflate(Resource.Layout.YourFragment, container, false);
 
-            View view = inflater.Inflate(Resource.Layout.ContactView, container, false);
+            View view = inflater.Inflate(Resource.Layout.ContactFragmentView, container, false);
             return view;
         }
 
@@ -43,7 +50,13 @@ namespace Eventos.Fragments
 
         public void FindViews()
         {
-
+            eventNameText = this.View.FindViewById<TextView>(Resource.Id.eventTitleTextContact);
+            eventDateText = this.View.FindViewById<TextView>(Resource.Id.eventDateTextContact);
+            eventDescriptionText = this.View.FindViewById<TextView>(Resource.Id.eventDescriptionTextContact);
+            eventHashTagText = this.View.FindViewById<TextView>(Resource.Id.eventHashtagContact);
+            eventEmailText = this.View.FindViewById<TextView>(Resource.Id.eventMailTextContact);
+            eventNumberText = this.View.FindViewById<TextView>(Resource.Id.eventPhoneNumbersContact);
+            eventAddressText = this.View.FindViewById<TextView>(Resource.Id.eventPlaceTextContact);
         }
 
         public void InstantiateDataService(DataService dataServiceInstance)
@@ -53,7 +66,12 @@ namespace Eventos.Fragments
 
         public void PopulateData()
         {
-
+            eventNameText.Text = dataServiceInstance.GetEvent().EventInformation.EventName;
+            eventAddressText.Text = dataServiceInstance.GetEvent().Place.Address;
+            eventDescriptionText.Text = dataServiceInstance.GetEvent().EventInformation.EventDescription;
+            eventHashTagText.Text = "#" + dataServiceInstance.GetEvent().EventInformation.EventName;
+            eventEmailText.Text = dataServiceInstance.GetEvent().EventInformation.EventName + "@gmail.com";
+            eventNumberText.Text = "+57 355 6879";
         }
     }
 }
